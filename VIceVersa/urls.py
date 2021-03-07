44995420a3d673063ffbca73798410e68db1978f
Vlados123sos <vlados123sos@gmail.com>
2021-03-07 20:25:56 +0300

    Add reversed text

--- VIceVersa/urls.py
+++ VIceVersa/urls.py
@@ -20,4 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main_page),
+    path('reversed',views.reverse,name='reverse')
 ]
--- VIceVersa/views.py
+++ VIceVersa/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
-def main_page(responce):
-    return render(responce,'home.html')
+def main_page(request):
+    return render(request,'home.html')
+def reverse(request):
+    user_text=request.GET['usertext']
+    reversed_text=user_text[::-1]
+    return render(request,'reverse.html',{'usertext':user_text,'reversedtext':reversed_text})
