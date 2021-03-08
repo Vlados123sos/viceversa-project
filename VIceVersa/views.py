5700366418f045d481514c0e02dffa7466d9899c
Vlados123sos <vlados123sos@gmail.com>
2021-03-08 14:21:36 +0300

    complete 3 exercise

--- VIceVersa/views.py
+++ VIceVersa/views.py
@@ -4,5 +4,7 @@ def main_page(request):
     return render(request,'home.html')
 def reverse(request):
     user_text=request.GET['usertext']
+    s=user_text.split()
+    sum_of_words=len(s)
     reversed_text=user_text[::-1]
-    return render(request,'reverse.html',{'usertext':user_text,'reversedtext':reversed_text})
+    return render(request,'reverse.html',{'usertext':user_text,'reversedtext':reversed_text,'sumwords':sum_of_words})
